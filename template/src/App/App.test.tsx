ae5888d3388a1f38c1522a3bda94c1f3c0bffc08
Ashley Clifton <randomashes@gmail.com>
2022-03-09 22:47:26 -0500

    Add jest-axe for unit testing accessibility

--- template/src/App/App.test.tsx
+++ template/src/App/App.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should render without axe errors', async () => {
+    const { container } = render(<App />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
 });
--- template/src/setupTests.ts
+++ template/src/setupTests.ts
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import { toHaveNoViolations } from 'jest-axe';
+
 import '@testing-library/jest-dom';
+
+expect.extend(toHaveNoViolations);
